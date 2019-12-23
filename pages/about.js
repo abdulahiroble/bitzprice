80d42bfeb54347f688b08281bb2abc6ec8c6a469
abdulahiroble <abdulahi_2610@hotmail.com>
2019-12-23 21:23:25 +0100

    created about page and changed index page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About BitzPrice</h1>
+      <p>Application to view Bitcoin prices</p>
+    </div>
+  );
+};
+
+export default About;
